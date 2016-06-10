e0c7d9445cec7c02b50b41cc5f3d975b4bcc9b19
Jakub Kubacki <kubacki.jk@gmail.com>
2016-06-10 20:39:03 +0200

    Fix auth

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -5,7 +5,7 @@ defmodule Rumbl.Auth do
     Keyword.fetch!(opts, :repo)
   end
 
-  def call(conn, opts) do
+  def call(conn, repo) do
     user_id = get_session(conn, :user_id)
     user = user_id && repo.get(Rumbl.User, user_id)
     assign(conn, :current_user, user)
