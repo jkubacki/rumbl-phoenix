cf89929ba2166b0c990231fc75fed2c1284392ec
Jakub Kubacki <kubacki.jk@gmail.com>
2016-06-10 20:44:17 +0200

    Use function plug instead of case in action
    Add plug to #index & #show

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -2,14 +2,11 @@ defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
   alias Rumbl.User
 
+  plug :authenticate when action in [:index, :show]
+
   def index(conn, _params) do
-    case authenticate(conn) do
-      %Plug.Conn{halted: true} ->
-        conn
-      conn ->
-        users = Repo.all(Rumbl.User)
-        render conn, "index.html", users: users
-    end
+    users = Repo.all(Rumbl.User)
+    render conn, "index.html", users: users
   end
 
   def show(conn, %{"id" => id}) do
@@ -34,7 +31,7 @@ defmodule Rumbl.UserController do
     end
   end
 
-  defp authenticate(conn) do
+  defp authenticate(conn, _opts) do
     if conn.assigns.current_user do
       conn
     else
