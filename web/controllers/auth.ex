258ac6e87adbd57da3c311d07e019407a9ece495
Jakub Kubacki <kubacki.jk@gmail.com>
2016-06-14 00:02:56 +0200

    Pass user token to client
    It will be used to authenticate via websocket

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -13,11 +13,11 @@ defmodule Rumbl.Auth do
 
     cond do
       user = conn.assigns[:current_user] ->
-        conn
+        put_current_user(conn, user)
       user = user_id && repo.get(Rumbl.User, user_id) ->
-        assign(conn, :current_user, user)
+        put_current_user(conn, user)
       true ->
-        assign(conn, :current_user, nil)
+        put_current_user(conn, nil)
     end
   end
 
@@ -37,6 +37,14 @@ defmodule Rumbl.Auth do
     |> assign(:current_user, user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
+  end
+
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
 
   def logout(conn) do
