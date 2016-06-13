a760f30c8521bfd2f96e923050c13b7efb7a7368
Jakub Kubacki <kubacki.jk@gmail.com>
2016-06-13 23:42:49 +0200

    Handle message from client and broadcast it further

--- web/channels/video_channel.ex
+++ web/channels/video_channel.ex
@@ -4,4 +4,14 @@ defmodule Rumbl.VideoChannel do
   def join("videos:" <> video_id, _params, socket) do
     {:ok, socket}
   end
+
+  def handle_in("new_annotation", params, socket) do
+    broadcast! socket, "new_annotation", %{
+      user: %{username: "annon"},
+      body: params["body"],
+      at: params["at"]
+    }
+
+    {:reply, :ok, socket}
+  end
 end
